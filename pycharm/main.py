81f607fef313068e9fed2da186418bc8fecfe4b3
Daniel Delgado <danieldel@ufl.edu>
2023-10-19 10:57:53 -0400

    feat: exception handling in ar script

--- pycharm/main.py
+++ pycharm/main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+b = 0 #simulated b > 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+try:
+    if(b < 1):
+        raise ValueError("B cannot be divided by zero")
+
+except ValueError as error:
+    print("The error we generated caught by exception: ", str(error))
+#
+
+str_list = ["apples","oranges","tomatoes"]
+for i in str_list:
+    print(i)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# print("apples" in str_list)
+# assert "mandarin" in str_list
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    if("mandarin" not in str_list):
+        raise ValueError("mandarin")
+except ValueError as error:
+    print("Array needs to contain word: ",str(error))
